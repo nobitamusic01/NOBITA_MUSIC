b939f8b68c691fffe905b410d99c2d7f4b238e86
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <abhaysinghchauhan2062@gmail.com>
2025-07-05 17:01:28 +0530

    Update play.py

--- NOBITA_MUSIC/utils/decorators/play.py
+++ NOBITA_MUSIC/utils/decorators/play.py
@@ -20,7 +20,7 @@ from NOBITA_MUSIC.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from VILLAIN_MUSIC.utils.inline import botplaylist_markup
+from NOBITA_MUSIC.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
